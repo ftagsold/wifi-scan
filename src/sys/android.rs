069e0f0b987a5ad30f0e457c412a96eef72ba6ee
Fabian Tagsold <fabian@tagsold.io>
2026-01-29 20:02:53 +0100

    android

--- src/sys/android.rs
+++ src/sys/android.rs
@@ -2,8 +2,6 @@ use crate::{Error, Result, Wifi};
 use jni::objects::{GlobalRef, JString, JValue};
 use jni::JavaVM;
 use std::sync::OnceLock;
-use std::thread::sleep;
-use std::time::Duration;
 
 pub static ANDROID_SCANNER: OnceLock<AndroidScanner> = OnceLock::new();
 
@@ -72,8 +70,6 @@ impl AndroidScanner {
             if size > 0 {
                 break (scan_results, size);
             }
-
-            sleep(Duration::from_millis(100));
         };
 
         let mut networks = Vec::new();
